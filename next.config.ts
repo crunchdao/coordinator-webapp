6cf52ad63c9afb8bfd8560898ce1714d9302b34e
boutrig abdennour <abdennour.boutrig@crunchdao.com>
2025-12-10 16:24:02 +0100

    fix(builder): set api url during the build phase

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,12 @@
 import type { NextConfig } from "next";
 
-const apiBaseUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
-
 const nextConfig: NextConfig = {
   poweredByHeader: false,
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: `${apiBaseUrl}/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000"}/:path*`,
       },
     ];
   },
