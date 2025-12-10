e2571f2b825104d7927d736c90df9e8abb675016
hunscuzzy <32163579+Hunscuzzy@users.noreply.github.com>
2025-12-10 16:23:15 +0100

    fix: projectIds

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const apiBaseUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+console.log(apiBaseUrl);
 
 const nextConfig: NextConfig = {
   poweredByHeader: false,
