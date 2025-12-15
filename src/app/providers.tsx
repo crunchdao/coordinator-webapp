29bda35c70da1d27473575058e64ed865f429b8b
hunscuzzy <32163579+Hunscuzzy@users.noreply.github.com>
2025-12-15 12:32:57 +0100

    feat(config): add version

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,9 @@
 "use client";
 import { ReactNode } from "react";
 import { TooltipProvider } from "@crunch-ui/core";
-import { useGlobalSettings } from "@/modules/settings/application/hooks/useGlobalSettings";
 import { SettingsProvider } from "@/modules/settings/application/context/settingsContext";
 
 const Providers: React.FC<{ children: ReactNode }> = ({ children }) => {
-  useGlobalSettings();
   return (
     <SettingsProvider>
       <TooltipProvider disableHoverableContent delayDuration={50}>
--- src/modules/settings/application/context/settingsContext.tsx
+++ src/modules/settings/application/context/settingsContext.tsx
@@ -1,17 +1,28 @@
-'use client';
+"use client";
 
-import React, { createContext, useContext, useMemo } from 'react';
+import React, { createContext, useContext, useMemo } from "react";
+import { config } from "@/utils/config";
+import { useGlobalSettings } from "../hooks/useGlobalSettings";
 
 interface SettingsContextType {
   isLocal: boolean;
+  version: string;
 }
 
-const SettingsContext = createContext<SettingsContextType | undefined>(undefined);
+const SettingsContext = createContext<SettingsContextType | undefined>(
+  undefined
+);
 
 export function SettingsProvider({ children }: { children: React.ReactNode }) {
-  const value = useMemo(() => ({
-    isLocal: process.env.NODE_ENV === 'development'
-  }), []);
+  useGlobalSettings();
+
+  const value = useMemo(
+    () => ({
+      isLocal: config.env === "development",
+      version: config.version,
+    }),
+    []
+  );
 
   return (
     <SettingsContext.Provider value={value}>
@@ -23,7 +34,7 @@ export function SettingsProvider({ children }: { children: React.ReactNode }) {
 export function useSettings() {
   const context = useContext(SettingsContext);
   if (!context) {
-    throw new Error('useSettings must be used within a SettingsProvider');
+    throw new Error("useSettings must be used within a SettingsProvider");
   }
   return context;
 }
