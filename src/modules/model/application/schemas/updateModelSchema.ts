d46d0fed29dd4af836e837420fc7c84d24c7163d
hunscuzzy <32163579+Hunscuzzy@users.noreply.github.com>
2025-12-18 12:48:44 +0100

    feat(model): update model

--- src/modules/model/application/schemas/updateModelSchema.ts
+++ src/modules/model/application/schemas/updateModelSchema.ts
@@ -2,5 +2,8 @@ import { z } from "zod";
 import { DesiredState } from "../../domain/types";
 
 export const updateModelSchema = z.object({
-  desiredState: z.nativeEnum(DesiredState),
+  desired_state: z.nativeEnum(DesiredState),
+  model_name: z.string().nullable().optional(),
+  cruncher_name: z.string().nullable().optional(),
+  files: z.array(z.instanceof(File)),
 });
