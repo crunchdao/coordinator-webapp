745901e918460ff7d7745afcf9f8eb2aa84ed69c
hunscuzzy <32163579+Hunscuzzy@users.noreply.github.com>
2026-01-06 17:40:21 +0100

    feat(auth): readonly only on need approval

---
+++ src/app/(authenticated)/(needApproval)/layout.tsx
@@ -0,0 +1,16 @@
+import { ReactElement } from "react";
+import { PendingStatusBanner } from "@/modules/auth/ui/pendingStatusBanner";
+import { ReadOnlyWrapper } from "@/modules/auth/ui/readOnlyWrapper";
+
+export default function AuthenticatedLayout({
+  children,
+}: {
+  children: ReactElement;
+}) {
+  return (
+    <>
+      <PendingStatusBanner />
+      <ReadOnlyWrapper>{children}</ReadOnlyWrapper>
+    </>
+  );
+}
--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -1,17 +1,10 @@
 import { ReactElement } from "react";
 import { AuthWrapper } from "@/modules/auth/ui/authWrapper";
-import { PendingStatusBanner } from "@/modules/auth/ui/pendingStatusBanner";
-import { ReadOnlyWrapper } from "@/modules/auth/ui/readOnlyWrapper";
 
 export default function AuthenticatedLayout({
   children,
 }: {
   children: ReactElement;
 }) {
-  return (
-    <AuthWrapper>
-      <PendingStatusBanner />
-      <ReadOnlyWrapper>{children}</ReadOnlyWrapper>
-    </AuthWrapper>
-  );
+  return <AuthWrapper>{children}</AuthWrapper>;
 }
