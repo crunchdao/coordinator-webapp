23d434ba90cb3e885203cfa4f10b18b02be08b22
hunscuzzy <32163579+Hunscuzzy@users.noreply.github.com>
2026-01-06 17:42:30 +0100

    feat(auth): readonly only on need approval

--- src/app/(authenticated)/(needApproval)/layout.tsx
+++ src/app/(authenticated)/(needApproval)/layout.tsx
@@ -2,7 +2,7 @@ import { ReactElement } from "react";
 import { PendingStatusBanner } from "@/modules/auth/ui/pendingStatusBanner";
 import { ReadOnlyWrapper } from "@/modules/auth/ui/readOnlyWrapper";
 
-export default function AuthenticatedLayout({
+export default function ApprovedLayout({
   children,
 }: {
   children: ReactElement;
