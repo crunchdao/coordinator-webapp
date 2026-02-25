bc2fcf023f3cd660ddc081a1fb23416a876c3706
hunscuzzy <32163579+Hunscuzzy@users.noreply.github.com>
2026-02-25 20:27:00 +0100

    feat: spinner center

--- apps/platform/src/modules/onboarding/ui/onboardingPanel.tsx
+++ apps/platform/src/modules/onboarding/ui/onboardingPanel.tsx
@@ -25,9 +25,9 @@ export function OnboardingPanel() {
 
   const StepComponent = currentStepInfo?.Component;
 
-  if (isLoading) {
+  if (!isLoading) {
     return (
-      <div className="flex items-center justify-center h-full">
+      <div className="flex flex-1 items-center justify-center h-full">
         <Spinner />
       </div>
     );
