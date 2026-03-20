357c0813c3739fed4f1d7b653bf013e2d7fe92e8
hunscuzzy <32163579+Hunscuzzy@users.noreply.github.com>
2026-03-20 12:25:45 +0100

    feat(coordinator): add button finish setup

--- apps/platform/src/modules/coordinator/ui/pendingStatusBanner.tsx
+++ apps/platform/src/modules/coordinator/ui/pendingStatusBanner.tsx
@@ -1,7 +1,9 @@
 "use client";
+import Link from "next/link";
 import { InfoCircle } from "@crunch-ui/icons";
+import { Alert, AlertDescription, AlertTitle, Button } from "@crunch-ui/core";
 import { useCoordinatorAuth } from "../application/context/coordinatorAuthContext";
-import { Alert, AlertDescription, AlertTitle } from "@crunch-ui/core";
+import { INTERNAL_LINKS } from "@/utils/routes";
 
 export function PendingStatusBanner() {
   const { isReadOnly } = useCoordinatorAuth();
@@ -12,10 +14,14 @@ export function PendingStatusBanner() {
     <Alert variant="warning" className="container mx-auto my-3">
       <InfoCircle />
       <AlertTitle>Application in Read-Only Mode</AlertTitle>
-      <AlertDescription>
-        Your coordinator registration is pending approval. You can browse but
-        cannot perform actions. We are going to notify you when your
-        registration is approved.
+      <AlertDescription className="flex flex-col gap-1">
+        <span>
+          Your coordinator registration is pending approval. You can browse but
+          cannot perform actions.
+        </span>
+        <Link href={INTERNAL_LINKS.ONBOARDING}>
+          <Button size="sm">Finish Setup</Button>
+        </Link>
       </AlertDescription>
     </Alert>
   );
